8e7ea33a44465c02ead6ef88c570132424213a68
Efren Cabrera <efren507.507@gmail.com>
2019-06-25 21:24:45 -0700

    Agrega línea para extraer llave de google maps desde variable de entorno

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
+import os
 from flask import Flask, render_template, send_from_directory
-app = Flask(__name__, root_path='', template_folder="web/templates", static_url_path='')
+app = Flask(__name__, root_path='',
+            template_folder="web/templates", static_url_path='')
+google_maps_key = os.environ["GOOGLE_MAPS_KEY"]
 
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('web/static/js/', path)
 
+
 @app.route('/css/<path:path>')
 def send_css(path):
     return send_from_directory('web/static/css/', path)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', google_maps_key=google_maps_key)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
