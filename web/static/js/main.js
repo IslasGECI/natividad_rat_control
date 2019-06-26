9b613e5ac63ec49613cf7cb4dcf843eb275c2f46
Efren Cabrera <efren507.507@gmail.com>
2019-06-26 08:42:24 -0700

    Agrega código para dibujar círculos sobre el mapa

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,7 +1,44 @@
 d3.select(window).on("load", () => {
-    var map = new google.maps.Map(d3.select("#map").node(), {
+    let map = new google.maps.Map(d3.select("#map").node(), {
         zoom: 16,
         center: new google.maps.LatLng(27.856601, -115.172974),
         mapTypeId: 'satellite'
+    });
+    d3.json("/positions", function (data) {
+        let overlay = new google.maps.OverlayView();        
+        overlay.onAdd = function () {
+            let layer = d3.select(this.getPanes().overlayLayer).append("div")
+                .attr("class", "traps");            
+            overlay.draw = function () {
+                let projection = this.getProjection(),
+                    padding = 10;
+                let marker = layer.selectAll("svg")
+                    .data(data)
+                    .each(transform)
+                    .enter().append("svg")
+                    .each(transform)
+                    .attr("class", "marker");                
+                marker.append("circle")
+                    .attr("r", 4.5)
+                    .attr("cx", padding)
+                    .attr("cy", padding);                
+                marker.append("text")
+                    .attr("x", padding + 7)
+                    .attr("y", padding)
+                    .attr("dy", ".31em")
+                    .attr("fill", "white")
+                    .attr("stroke", "black")                    
+                    .attr("font-size", "1.5em")
+                    .text(function (d) { return d["id trampa"]; });
+                function transform(d) {                    
+                    d = new google.maps.LatLng(d.latitude, d.longitude);
+                    d = projection.fromLatLngToDivPixel(d);                    
+                    return d3.select(this)                        
+                        .style("left", (d.x - padding) + "px")
+                        .style("top", (d.y - padding) + "px");
+                }
+            };
+        };
+        overlay.setMap(map);
     });
 });
